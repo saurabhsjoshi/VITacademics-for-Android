f39e9d69c1c50fc389c1590a22ee829a87c08b4c
Saurabh Joshi <battlex2010@yahoo.com>
2014-04-30 18:09:09 +0800

    Added new Settings Activity

---
+++ VITacademics/src/main/java/com/collegecode/PrefsUI/PrefsCampus.java
@@ -0,0 +1,49 @@
+package com.collegecode.PrefsUI;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RadioButton;
+
+import com.collegecode.VITacademics.R;
+import com.collegecode.objects.DataHandler;
+
+/**
+ * Created by saurabh on 4/30/14.
+ */
+public class PrefsCampus extends DialogPreference {
+    RadioButton radVel, radChen;
+    DataHandler dat;
+
+    public PrefsCampus(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setDialogLayoutResource(R.layout.prefdialog_campus);
+        dat = new DataHandler(context);
+    }
+
+    @Override
+    public void onBindDialogView(View view){
+        super.onBindDialogView(view);
+
+        radVel = (RadioButton) view.findViewById(R.id.radioVel);
+        radChen = (RadioButton) view.findViewById(R.id.radioChen);
+
+        if(dat.isVellore())
+            radVel.setChecked(true);
+        else
+            radChen.setChecked(true);
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if(positiveResult){
+            if(radVel.isChecked())
+                dat.saveCampus(true);
+            else
+                dat.saveCampus(false);
+        }
+    }
+
+}
