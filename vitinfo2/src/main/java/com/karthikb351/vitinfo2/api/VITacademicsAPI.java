3797f627543a6f1cc9a26ded6cd8ded9f6f3a882
Karthik Balakrishnan <karthikb351@gmail.com>
2015-02-22 12:16:49 +0530

    Basic API-Service framework
    - Added retrofit client and service

--- vitinfo2/src/main/java/com/karthikb351/vitinfo2/api/VITacademicsAPI.java
+++ vitinfo2/src/main/java/com/karthikb351/vitinfo2/api/VITacademicsAPI.java
@@ -1,8 +1,33 @@
 package com.karthikb351.vitinfo2.api;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
+
 /**
  * Created by karthikbalakrishnan on 05/02/15.
  */
 public class VITacademicsAPI {
 
+    private static final String BASE_URL = "https://vitacademics-dev.herokuapp.com";
+    private VITacademicsService service;
+
+    public VITacademicsAPI() {
+        Gson gson = new GsonBuilder().create();
+
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .setEndpoint(BASE_URL)
+                .setConverter(new GsonConverter(gson))
+                .build();
+
+        service = restAdapter.create(VITacademicsService.class);
+    }
+
+    public VITacademicsService getApiService()
+    {
+        return service;
+    }
 }
