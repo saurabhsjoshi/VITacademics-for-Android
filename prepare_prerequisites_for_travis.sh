25c2e80e7b7467bb52ad3d37be84ecaa69a0d233
aashrai <ashrair@gmail.com>
2015-03-21 16:39:22 +0530

    updated .travis.yml

--- prepare_prerequisites_for_travis.sh
+++ prepare_prerequisites_for_travis.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-touch vitinfo2/src/main/java/com/karthikb351/vitinfo2/objects/Secret.java
+touch vitinfo2/src/main/java/com/karthikb351/vitinfo2/Secret.java
 echo "package com.karthikb351.vitinfo2.objects;
 
 public class Secret {
     public static String backup_password = \"ANYSTRINGHERE\";
-}" >  vitinfo2/src/main/java/com/karthikb351/vitinfo2/objects/Secret.java
+}" >  vitinfo2/src/main/java/com/karthikb351/vitinfo2/Secret.java
