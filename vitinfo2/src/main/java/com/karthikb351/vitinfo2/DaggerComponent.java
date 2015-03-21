4826ec4d857c9728cc55139bef7140693b8cebe6
aashrai <ashrair@gmail.com>
2015-03-21 08:51:05 +0530

    added project lombok

---
+++ vitinfo2/src/main/java/com/karthikb351/vitinfo2/DaggerComponent.java
@@ -0,0 +1,17 @@
+package com.karthikb351.vitinfo2;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by aashrai on 21/3/15.
+ */
+@Singleton
+@Component()
+public class DaggerComponent {
+
+
+    public class Initializer
+
+}
