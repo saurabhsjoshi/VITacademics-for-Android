3166e700944918cc8f514fce3cd28cf2e1dd7555
aashrai <ashrair@gmail.com>
2015-04-01 11:25:01 +0530

    Added support for crashlytics(twitter fabric)

--- vitinfo2/src/main/java/com/karthikb351/vitinfo2/VITacademics.java
+++ vitinfo2/src/main/java/com/karthikb351/vitinfo2/VITacademics.java
@@ -2,12 +2,14 @@ package com.karthikb351.vitinfo2;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.karthikb351.vitinfo2.api.VITacademicsAPI;
 import com.karthikb351.vitinfo2.bus.APIErrorEvent;
 import com.karthikb351.vitinfo2.bus.BusProvider;
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
 
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 /**
@@ -21,6 +23,7 @@ public class VITacademics extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
@@ -36,32 +39,35 @@ public class VITacademics extends Application {
         mBus.register(this); //listen for "global" events
     }
 
+    @Subscribe
+    public void onAPIErrorEvent(APIErrorEvent event) {
+        Timber.e("An error as occured: %s", event.getMessage());
+    }
 
-    /** A tree which logs important information for crash reporting. */
+    /**
+     * A tree which logs important information for crash reporting.
+     */
     private static class CrashReportingTree extends Timber.HollowTree {
-        @Override public void i(String message, Object... args) {
-            // TODO e.g., Crashlytics.log(String.format(message, args));
+        @Override
+        public void i(String message, Object... args) {
+            Crashlytics.log(String.format(message, args));
         }
 
-        @Override public void i(Throwable t, String message, Object... args) {
+        @Override
+        public void i(Throwable t, String message, Object... args) {
             i(message, args); // Just add to the log.
         }
 
-        @Override public void e(String message, Object... args) {
+        @Override
+        public void e(String message, Object... args) {
             i("ERROR: " + message, args); // Just add to the log.
         }
 
-        @Override public void e(Throwable t, String message, Object... args) {
+        @Override
+        public void e(Throwable t, String message, Object... args) {
             e(message, args);
 
-            // TODO e.g., Crashlytics.logException(t);
+            Crashlytics.logException(t);
         }
-    }
-
-
-
-    @Subscribe
-    public void onAPIErrorEvent(APIErrorEvent event){
-        Timber.e("An error as occured: %s", event.getMessage());
     }
 }
