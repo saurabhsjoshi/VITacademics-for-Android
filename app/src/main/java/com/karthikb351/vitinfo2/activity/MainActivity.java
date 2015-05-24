c8593abc5685f03f747b57fda715066ce17a0cf8
aneesh-neelam <neelam.aneesh@gmail.com>
2015-05-24 22:04:15 +0530

    Started new UI
    * MainActivity + Fragment (temp) recreated
    * Layout recreated

--- app/src/main/java/com/karthikb351/vitinfo2/activity/MainActivity.java
+++ app/src/main/java/com/karthikb351/vitinfo2/activity/MainActivity.java
@@ -1,162 +1,58 @@
+/*
+ * VITacademics
+ * Copyright (C) 2015  Aneesh Neelam <neelam.aneesh@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.karthikb351.vitinfo2.activity;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.karthikb351.vitinfo2.R;
-import com.karthikb351.vitinfo2.bus.BusProvider;
-import com.karthikb351.vitinfo2.bus.events.GetShareTokenEvent;
-import com.karthikb351.vitinfo2.bus.events.LoginCompleteEvent;
-import com.karthikb351.vitinfo2.bus.events.LoginEvent;
-import com.karthikb351.vitinfo2.bus.events.RefreshDataEvent;
-import com.squareup.otto.Bus;
-import com.squareup.otto.Subscribe;
 
-/**
- * Created by karthikbalakrishnan on 19/03/15.
- */
-public class MainActivity extends Activity {
-
-    Bus mBus = BusProvider.getInstance();
-
-    public static final String PROPERTY_REG_ID = "registration_id";
-    private static final String PROPERTY_APP_VERSION = "appVersion";
-    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button login = (Button)findViewById(R.id.login);
-        Button refresh = (Button)findViewById(R.id.refresh);
-        Button register = (Button)findViewById(R.id.shareToken);
-
-        login.setOnClickListener(ocl);
-        refresh.setOnClickListener(ocl);
-        register.setOnClickListener(ocl);
-
-    }
-
-    View.OnClickListener ocl = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-           switch (v.getId()){
-
-               case R.id.login:
-                   mBus.post(new LoginEvent());
-                   break;
-
-               case R.id.refresh:
-                   mBus.post(new RefreshDataEvent());
-
-                   break;
-
-               case R.id.shareToken:
-                   mBus.post(new GetShareTokenEvent());
-                   break;
-
-           }
-        }
-    };
-
-    @Subscribe
-    public void onLoginCompleteEvent(LoginCompleteEvent event)
-    {
-        Toast.makeText(MainActivity.this, "Reg:"+event.getLoginResponse().getRegno()+" Status:"+event.getLoginResponse().getStatus().getMessage(), Toast.LENGTH_SHORT).show();
     }
 
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        mBus.register(this);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mBus.unregister(this);
-    }
-
-    /**
-     * Stores the registration id, app versionCode, and expiration time in the application's
-     * {@code SharedPreferences}.
-     *
-     * @param context application's context.
-     * @param regId registration id
-     */
-    private void setRegistrationId(Context context, String regId) {
-        final SharedPreferences prefs = getGCMPreferences(context);
-        int appVersion = getAppVersion(context);
-        //Log.v(TAG, "Saving regId on app version " + appVersion);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(PROPERTY_REG_ID, regId);
-        editor.putInt(PROPERTY_APP_VERSION, appVersion);
-        editor.commit();
-    }
-
-    /**
-     * Gets the current registration id for application on GCM service.
-     * <p>
-     * If result is empty, the registration has failed.
-     *
-     * @return registration id, or empty string if the registration is not
-     *         complete.
-     */
-    private String getRegistrationId(Context context) {
-        final SharedPreferences prefs = getGCMPreferences(context);
-        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
-        if (registrationId.length() == 0) {
-            //Log.v(TAG, "Registration not found.");
-            return "";
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
-        // check if app was updated; if so, it must clear registration id to
-        // avoid a race condition if GCM sends a message
-        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
-        int currentVersion = getAppVersion(context);
-        if (registeredVersion != currentVersion) {
-            //Log.v(TAG, "App version changed or registration expired.");
-            return "";
-        }
-        return registrationId;
-    }
 
-    /**
-     * @return Application's version code from the {@code PackageManager}.
-     */
-    private static int getAppVersion(Context context) {
-        try {
-            PackageInfo packageInfo = context.getPackageManager()
-                    .getPackageInfo(context.getPackageName(), 0);
-            return packageInfo.versionCode;
-        } catch (PackageManager.NameNotFoundException e) {
-            // should never happen
-            throw new RuntimeException("Could not get package name: " + e);
-        }
+        return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * @return Application's {@code SharedPreferences}.
-     */
-    private SharedPreferences getGCMPreferences(Context context) {
-        return getSharedPreferences(MainActivity.class.getSimpleName(), Context.MODE_PRIVATE);
-    }
-
 }
