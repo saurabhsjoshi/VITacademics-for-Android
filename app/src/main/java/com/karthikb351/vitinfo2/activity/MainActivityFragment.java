b59b1f768ee6e8fab2cb666592d381467cf991cd
aneesh-neelam <neelam.aneesh@gmail.com>
2015-05-24 22:25:52 +0530

    Reformatted Code

--- app/src/main/java/com/karthikb351/vitinfo2/activity/MainActivityFragment.java
+++ app/src/main/java/com/karthikb351/vitinfo2/activity/MainActivityFragment.java
@@ -32,8 +32,7 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
