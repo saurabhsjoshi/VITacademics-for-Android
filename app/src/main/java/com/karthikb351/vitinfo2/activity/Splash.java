d52e04ddfacde3f14b44bbaf5b359aacb532bd3a
Gaurav <gauravagerwala@gmail.com>
2015-06-28 15:40:01 +0530

    Splash modified

--- app/src/main/java/com/karthikb351/vitinfo2/activity/Splash.java
+++ app/src/main/java/com/karthikb351/vitinfo2/activity/Splash.java
@@ -1,5 +1,21 @@
 package com.karthikb351.vitinfo2.activity;
-
+/*
+ * VITacademics
+ * Copyright (C) 2015  Gaurav Agerwala <gauravagerwala@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import android.app.Activity;
 import android.content.Intent;
 import android.media.AudioManager;
@@ -8,9 +24,6 @@ import android.os.Bundle;
 
 import com.karthikb351.vitinfo2.R;
 
-/**
- * Created by gaurav on 28/6/15.
- */
 public class Splash extends Activity {
 
     SoundPool sp;
