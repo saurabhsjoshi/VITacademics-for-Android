saurabhsjoshi/VITacademics-for-Android

commit d79b73266e83277aa87d100e4bccd1acf37f0829

Refactor XML
Changes to follow current naming convention.

app/src/main/java/com/karthikb351/vitinfo2/contract/Friend.java
@@ -52,7 +52,7 @@ public class Friend extends SugarRecord<Friend> {
     private String semester;
 
     @Expose
-    @SerializedName("courses")
+    @SerializedName("course")
     private FriendCourse[] courses;
 
     @Expose

app/src/main/java/com/karthikb351/vitinfo2/activity/LoginActivity.java
@@ -81,12 +81,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.bLogin:
+            case R.id.button_login:
                 refreshStatus = 0;
                 // TODO get values and fill in below code
                 // new Network(LoginActivity.this, ).refreshAll();
                 break;
-            case R.id.etDob:
+            case R.id.input_dob:
                 showDatePicker(v);
                 break;
         }
@@ -113,13 +113,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     public void initializeLayouts() {
-        editTextRegisterNumber = (EditText) findViewById(R.id.etRegNo);
-        editTextDateOfBirth = (EditText) findViewById(R.id.etDob);
-        editTextMobileNumber = (EditText) findViewById(R.id.etPhone);
-        radioGroupCampus = (RadioGroup) findViewById(R.id.rgCampus);
-        radioVelloreCampus = (RadioButton) findViewById(R.id.rbVellore);
-        radioChennaiCampus = (RadioButton) findViewById(R.id.rbChennai);
-        buttonLogin = (Button) findViewById(R.id.bLogin);
+        editTextRegisterNumber = (EditText) findViewById(R.id.input_reg_no);
+        editTextDateOfBirth = (EditText) findViewById(R.id.input_dob);
+        editTextMobileNumber = (EditText) findViewById(R.id.input_phone_no);
+        radioGroupCampus = (RadioGroup) findViewById(R.id.select_campus);
+        radioVelloreCampus = (RadioButton) findViewById(R.id.select_vellore);
+        radioChennaiCampus = (RadioButton) findViewById(R.id.select_chennai);
+        buttonLogin = (Button) findViewById(R.id.button_login);
         buttonLogin.setOnClickListener(this);
         editTextDateOfBirth.setOnClickListener(this);
 
@@ -128,7 +128,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
                 switch (checkedId) {
-                    case R.id.rbChennai:
+                    case R.id.select_chennai:
                         editTextMobileNumber.setFocusable(false);
                         break;
 

app/src/main/java/com/karthikb351/vitinfo2/adapter/NavigationDrawerAdapter.java
@@ -48,13 +48,13 @@ public class NavigationDrawerAdapter extends ArrayAdapter<String> {
         if (view == null) {
             view = LayoutInflater.from(context).inflate(R.layout.drawer_menu_item, parent, false);
             holder = new ViewHolder();
-            holder.drawerIcon = (ImageView) view.findViewById(R.id.drawerIcon);
-            holder.drawerText = (TextView) view.findViewById(R.id.drawerText);
+            holder.drawerIcon = (ImageView) view.findViewById(R.id.drawer_icon);
+            holder.drawerText = (TextView) view.findViewById(R.id.drawer_text);
             view.setTag(holder);
         } else
             holder = (ViewHolder) view.getTag();
 
-        //    holder.drawerIcon.setImageResource(context.getResources().getIdentifier(objects.get(position),"drawable",context.getPackageName()));
+        //    holder.drawer_icon.setImageResource(context.getResources().getIdentifier(objects.get(position),"drawable",context.getPackageName()));
         holder.drawerText.setText(objects.get(position));
         return view;
     }

app/src/main/java/com/karthikb351/vitinfo2/fragment/courses/CourseListAdapter.java
@@ -45,7 +45,7 @@ public class CourseListAdapter extends RecyclerView.Adapter<CourseListAdapter.Co
 
     @Override
     public CourseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        android.support.v7.widget.CardView rootcard = (android.support.v7.widget.CardView) LayoutInflater.from(context).inflate(R.layout.course_card, parent, false);
+        android.support.v7.widget.CardView rootcard = (android.support.v7.widget.CardView) LayoutInflater.from(context).inflate(R.layout.card_course, parent, false);
         return new CourseViewHolder(rootcard);
     }
 
@@ -78,12 +78,12 @@ public class CourseListAdapter extends RecyclerView.Adapter<CourseListAdapter.Co
         public ProgressBar pbAttendance;
         public CourseViewHolder(View v) {
             super(v);
-            courseName = (TextView) v.findViewById(R.id.tvCourseName);
-            courseCode = (TextView) v.findViewById(R.id.tvCourseCode);
-            Attendance = (TextView)v.findViewById(R.id.tvAttendance);
-            Slot = (TextView)v.findViewById(R.id.tvSlot);
-            Venue = (TextView)v.findViewById(R.id.tvVenue);
-            pbAttendance = (ProgressBar)v.findViewById(R.id.pbAttendance);
+            courseName = (TextView) v.findViewById(R.id.tv_course_name);
+            courseCode = (TextView) v.findViewById(R.id.tv_course_code);
+            Attendance = (TextView)v.findViewById(R.id.tv_attendance);
+            Slot = (TextView)v.findViewById(R.id.tv_slot);
+            Venue = (TextView)v.findViewById(R.id.tv_venue);
+            pbAttendance = (ProgressBar)v.findViewById(R.id.process_bar_attendance);
             pbAttendance.setMax(100);
             v.setOnClickListener(this);
         }

app/src/main/java/com/karthikb351/vitinfo2/fragment/courses/CoursesFragment.java
@@ -55,7 +55,7 @@ public class CoursesFragment extends Fragment {
         courses = new ArrayList<>();
         View view=inflater.inflate(R.layout.courses,container,false);
         courseListAdapter=new CourseListAdapter(getActivity(),courses);
-        recyclerView=(RecyclerView)view.findViewById(R.id.rvCourses);
+        recyclerView=(RecyclerView)view.findViewById(R.id.recycler_view_courses);
         courseListAdapter.setOnclickListener(new RecyclerViewOnClickListener<Course>() {
             @Override
             public void onItemClick(Course data) {

app/src/main/java/com/karthikb351/vitinfo2/fragment/friends/FriendsFragment.java
@@ -51,11 +51,11 @@ public class FriendsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
     {
-        View view = inflater.inflate(R.layout.friends_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
         friends = new ArrayList<Friend>();
         RecyclerView.LayoutManager friendsLayoutManager = new LinearLayoutManager(getActivity());
         adapter = new FriendsListAdapter(getActivity(),friends);
-        friendsRecyclerView = (RecyclerView)view.findViewById(R.id.rvFriends);
+        friendsRecyclerView = (RecyclerView)view.findViewById(R.id.recycler_view_friends);
         friendsRecyclerView.setLayoutManager(friendsLayoutManager);
         friendsRecyclerView.setAdapter(adapter);
         adapter.setOnclickListener(new RecyclerViewOnClickListener<Friend>() {

app/src/main/java/com/karthikb351/vitinfo2/fragment/friends/FriendsListAdapter.java
@@ -48,7 +48,7 @@ public class FriendsListAdapter extends RecyclerView.Adapter<FriendsListAdapter.
     public FriendsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.
                 from(parent.getContext()).
-                inflate(R.layout.freinds_card, parent, false);
+                inflate(R.layout.card_friends, parent, false);
 
         return new FriendsViewHolder(itemView);
     }
@@ -77,9 +77,9 @@ public class FriendsListAdapter extends RecyclerView.Adapter<FriendsListAdapter.
         public ImageView FriendImage;
         public FriendsViewHolder(View itemView) {
             super(itemView);
-            FriendRegNo=(TextView)itemView.findViewById(R.id.tvFriendRegistrationNumber);
-            FriendName=(TextView)itemView.findViewById(R.id.tvFriendName);
-            FriendImage=(ImageView)itemView.findViewById(R.id.FriendPhoto);
+            FriendRegNo=(TextView)itemView.findViewById(R.id.tv_friend_reg_no);
+            FriendName=(TextView)itemView.findViewById(R.id.tv_friend_name);
+            FriendImage=(ImageView)itemView.findViewById(R.id.photo_friend);
         }
         public  void onClick(View v)
         {

app/src/main/java/com/karthikb351/vitinfo2/fragment/settings/SettingsFragment.java
@@ -46,7 +46,7 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.settings_list,container,false);
-        listView=(ListView)view.findViewById(R.id.lvSettings);
+        listView=(ListView)view.findViewById(R.id.list_view_settings);
         settingTopics.add("Licenses");
         settingTopics.add("Contributors");
         settingTopics.add("Log Out");

commit 0aac76cf429862ec88877b36c822b7fb282e6e10

Fixed Friend GSON annotation.
* Fixed regression caused in d79b73266e83277aa87d100e4bccd1acf37f0829
* @CoderHam: I told you to fix this

app/src/main/java/com/karthikb351/vitinfo2/contract/Friend.java
@@ -52,7 +52,7 @@ public class Friend extends SugarRecord<Friend> {
     private String semester;
 
     @Expose
-    @SerializedName("course")
+    @SerializedName("courses")
     private FriendCourse[] courses;
 
     @Expose