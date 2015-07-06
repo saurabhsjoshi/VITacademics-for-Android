e1d93d33804c3752da90e816bbed146e783c034b
pulkit <pulkit.16296@gmail.com>
2015-07-06 21:15:35 +0530

    grade card fix

--- app/src/main/java/com/karthikb351/vitinfo2/fragment/SettingsFragment.java
+++ app/src/main/java/com/karthikb351/vitinfo2/fragment/SettingsFragment.java
@@ -21,8 +21,6 @@ package com.karthikb351.vitinfo2.fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
