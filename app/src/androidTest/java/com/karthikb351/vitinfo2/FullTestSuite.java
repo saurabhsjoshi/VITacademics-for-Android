1412ba82fa817e792027b9f69b57bf9d2aea2c62
aneesh-neelam <neelam.aneesh@gmail.com>
2015-07-08 16:21:53 +0530

    Android Tests for JSON parsing

---
+++ app/src/androidTest/java/com/karthikb351/vitinfo2/FullTestSuite.java
@@ -0,0 +1,37 @@
+/*
+ * VITacademics
+ * Copyright (C) 2015  Aneesh Neelam <neelam.aneesh@gmail.com>
+ *
+ * This file is part of VITacademics.
+ * VITacademics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VITacademics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VITacademics.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.karthikb351.vitinfo2;
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public FullTestSuite() {
+        super();
+    }
+
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere()
+                .build();
+    }
+}
---
+++ app/src/androidTest/java/com/karthikb351/vitinfo2/testJSONParser.java
@@ -0,0 +1,64 @@
+/*
+ * VITacademics
+ * Copyright (C) 2015  Aneesh Neelam <neelam.aneesh@gmail.com>
+ *
+ * This file is part of VITacademics.
+ * VITacademics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VITacademics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VITacademics.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.karthikb351.vitinfo2;
+
+import android.test.AndroidTestCase;
+
+import com.google.gson.Gson;
+
+public class testJSONParser extends AndroidTestCase {
+
+    private Gson gson;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        gson = new Gson();
+    }
+
+    public void testSystemJSON() {
+
+    }
+
+    public void testLoginJSON() {
+
+    }
+
+    public void testRefreshJSON() {
+
+    }
+
+    public void testGradesJSON() {
+
+    }
+
+    public void testTokenJSON() {
+
+    }
+
+    public void testShareJSON() {
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
