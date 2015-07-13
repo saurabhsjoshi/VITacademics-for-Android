7eab9b1782864d634c8eb137ebf474dc03fa5600
aneesh-neelam <neelam.aneesh@gmail.com>
2015-07-13 17:56:42 +0530

    Fixed Courses Fragment
    * TODO redesign of card
    * TODO OnClick to open CourseDetails

--- app/src/main/java/com/karthikb351/vitinfo2/fragment/courses/CoursesFragment.java
+++ app/src/main/java/com/karthikb351/vitinfo2/fragment/courses/CoursesFragment.java
@@ -57,7 +57,7 @@ public class CoursesFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.courses, container, false);
+        rootView = inflater.inflate(R.layout.fragment_courses, container, false);
         initialize();
         return rootView;
     }
