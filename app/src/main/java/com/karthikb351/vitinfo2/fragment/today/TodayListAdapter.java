d913e793d06bbcdea2b4084001197d1c87369a2c
aneesh-neelam <neelam.aneesh@gmail.com>
2015-07-14 18:28:45 +0530

    Using plurals for Hours and Minites left
    * Added plurals.xml

--- app/src/main/java/com/karthikb351/vitinfo2/fragment/today/TodayListAdapter.java
+++ app/src/main/java/com/karthikb351/vitinfo2/fragment/today/TodayListAdapter.java
@@ -102,11 +102,11 @@ public class TodayListAdapter extends RecyclerView.Adapter<TodayListAdapter.Toda
             hours = (int) TimeUnit.MILLISECONDS.toHours(diff);
             minutes = (int) (TimeUnit.MILLISECONDS.toMinutes(diff) - TimeUnit.HOURS.toMinutes(hours));
             if (hours == 0) {
-                return Integer.toString(minutes) + " " + context.getString(R.string.today_course_timing_minutes) + " " + context.getString(R.string.today_course_timing_later);
+                return context.getResources().getQuantityString(R.plurals.today_course_timing_minute, minutes) + context.getString(R.string.today_course_timing_later);
             } else if (minutes == 0) {
-                return Integer.toString(hours) + " " + context.getString(R.string.today_course_timing_hours) + " " + context.getString(R.string.today_course_timing_later);
+                return context.getResources().getQuantityString(R.plurals.today_course_timing_hour, hours) + context.getString(R.string.today_course_timing_later);
             } else {
-                return Integer.toString(hours) + " " + context.getString(R.string.today_course_timing_hours) + " " + context.getString(R.string.today_course_timing_and) + " " + Integer.toString(minutes) + " " + context.getString(R.string.today_course_timing_minutes) + " " + context.getString(R.string.today_course_timing_later);
+                return context.getResources().getQuantityString(R.plurals.today_course_timing_hour, hours) + context.getString(R.string.today_course_timing_and) + context.getResources().getQuantityString(R.plurals.today_course_timing_minute, minutes) + context.getString(R.string.today_course_timing_later);
             }
         } else {
             return context.getString(R.string.today_course_timing_right_now);
