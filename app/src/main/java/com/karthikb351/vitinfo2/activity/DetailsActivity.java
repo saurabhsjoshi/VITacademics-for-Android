eab325bf0ef7e97dfb2edbbbe69cf683689c213e
Saurabh Joshi <battlex2010@yahoo.com>
2015-07-18 12:16:48 +0800

    Fixed back arrow behaviour in DetailsActivity

--- app/src/main/java/com/karthikb351/vitinfo2/activity/DetailsActivity.java
+++ app/src/main/java/com/karthikb351/vitinfo2/activity/DetailsActivity.java
@@ -32,6 +32,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.karthikb351.vitinfo2.MainApplication;
@@ -73,8 +74,20 @@ public class DetailsActivity extends AppCompatActivity {
 
         final ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private class LoadCourseTask extends AsyncTask<Integer, Void, Course> {
@@ -124,4 +137,6 @@ public class DetailsActivity extends AppCompatActivity {
             return foundCourse;
         }
     }
+
+
 }
